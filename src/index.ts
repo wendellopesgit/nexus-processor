38456f52cbeeaf6f0fd2231941922c9e47b0ddf6
wendellopesgit <wls.lopes@gmail.com>
2025-07-11 16:57:15 -0300

    feat: add rabbitmq consumer and index to apply tests

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-// eslint-disable-next-line no-console
-console.log('Hello, Nexus Processor!');
+import { logger } from '@utils/logger.js';
+import RabbitMQConsumer from 'rabbitmq/consumer.js';
+
+const consumer = new RabbitMQConsumer('my_queue');
+
+async function main() {
+  await consumer.connect();
+
+  await consumer.consume(async (msg) => {
+    if (msg) {
+      logger.info('Received message:', msg.content.toString());
+    }
+  });
+
+  setTimeout(async () => {
+    await consumer.close();
+  }, 5000);
+}
+
+main().catch((error) => {
+  logger.error('Error in main:', error);
+  process.exit(1);
+});
---
+++ src/rabbitmq/consumer.ts
@@ -0,0 +1,66 @@
+import { logger } from '@utils/logger.js';
+import { ChannelModel, ConfirmChannel, connect, ConsumeMessage } from 'amqplib';
+
+class RabbitMQConsumer {
+  private connection: ChannelModel | null = null;
+  private channel: ConfirmChannel | null = null;
+
+  constructor(private queueName: string) {}
+
+  async connect(): Promise<void> {
+    try {
+      this.connection = await connect(process.env.RABBITMQ_URL as string);
+      this.channel = await this.connection.createConfirmChannel();
+
+      await this.channel.assertQueue(this.queueName, { durable: true });
+
+      logger.info(`Connected to RabbitMQ and listening to queue: ${this.queueName}`);
+    } catch (error) {
+      logger.error('Error connecting to RabbitMQ:', error);
+      throw error;
+    }
+  }
+
+  async consume(handler: (msg: ConsumeMessage | null) => Promise<void>): Promise<void> {
+    if (!this.channel) {
+      throw new Error('Channel not initialized. Call connect() first.');
+    }
+
+    await this.channel.consume(
+      this.queueName,
+      async (msg) => {
+        try {
+          await handler(msg);
+          if (msg && this.channel) {
+            this.channel.ack(msg);
+          }
+        } catch (err) {
+          logger.error('Error processing message:', err);
+          if (msg && this.channel) {
+            this.channel.nack(msg);
+          }
+        }
+      },
+      { noAck: false },
+    );
+  }
+
+  async close(): Promise<void> {
+    try {
+      if (this.channel) {
+        await this.channel.close();
+        this.channel = null;
+      }
+      if (this.connection) {
+        await this.connection.close();
+        this.connection = null;
+      }
+      logger.info('RabbitMQ connection closed');
+    } catch (error) {
+      logger.error('Error closing RabbitMQ connection:', error);
+      throw error;
+    }
+  }
+}
+
+export default RabbitMQConsumer;

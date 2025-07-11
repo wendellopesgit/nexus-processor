301f0e49e7d15f7c291ec484bf0830ba154e0c17
wendellopesgit <wls.lopes@gmail.com>
2025-07-11 12:52:54 -0300

    fix: eslint version settings

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import eslint from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs['recommended'].rules,
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'coverage/',
+      '*.log',
+      '**/*.d.ts',
+      '.husky/',
+      '.vscode/',
+      '**/__tests__/__snapshots__/',
+    ],
+  },
+];
